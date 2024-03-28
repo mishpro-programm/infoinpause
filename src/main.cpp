c020326f3d074a5de90340e4fe2dc9a3649ae1ab
mishpro <tabatcikov2010@gmail.com>
2024-03-28 22:14:38 +0700

    changed almost everything

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,18 @@ using namespace geode::prelude;
 
 #include <Geode/modify/PauseLayer.hpp>
 #include <Geode/modify/PlayLayer.hpp>
-//#include <Geode/modify/ProfilePage.hpp>
-//#include <Geode/modify/InfoLayer.hpp>
 #include <Geode/modify/CCLayer.hpp>
+#include <Geode/modify/LevelInfoLayer.hpp>
 class $modify(MyPauseLayer, PauseLayer) {
 	void customSetup(){
 		PauseLayer::customSetup();
 		auto menu = this->getChildByID("right-button-menu");
 		auto sprite = CCSprite::createWithSpriteFrameName("GJ_chatBtn_001.png");
 		sprite->setScale(0.65f);
-		//auto sprite = ButtonSprite::create("Info");
 		auto btn = CCMenuItemSpriteExtra::create(sprite, this, menu_selector(MyPauseLayer::onLevelInfo));
 		menu->setLayout(ColumnLayout::create()->setAxisAlignment(AxisAlignment::End));
 		menu->addChild(btn);
 		btn->setID("level-info-button");
-		//btn->setScale(0.675f, 0.675f);
 		menu->updateLayout();
 	}
 	void onLevelInfo(CCObject* sender){
@@ -28,7 +25,6 @@ class $modify(MyPauseLayer, PauseLayer) {
 			return;
 		}
 		auto level = playLayer->m_level;
-		log::error("userid: {}, accountid: {}", level->m_userID.value(), level->m_accountID.value());
 		auto infoLayer = InfoLayer::create(level, nullptr, nullptr);
 		infoLayer->setZOrder(11);
 		infoLayer->setScale(0.f);
@@ -38,21 +34,8 @@ class $modify(MyPauseLayer, PauseLayer) {
 		auto scene = CCDirector::sharedDirector()->getRunningScene();
 		scene->addChild(infoLayer);
 	}
-	void onExitH(){
-		auto* arr = this->getParent()->getChildren();
-		for(size_t i = arr->count(); i>0; i--){
-			auto* obj = arr->objectAtIndex(i-1);
-			if(auto* alert = typeinfo_cast<FLAlertLayer*>(obj)){
-				alert->retain();
-				alert->keyBackClicked();
-				Loader::get()->queueInMainThread([alert] {
-					alert->release();
-				});
-			}
-		}
-		PauseLayer::onExit();
-	}
 };
+//i copied this code from globed (tysm dankmeme)
 class $modify(FixedPlayLayer, PlayLayer) {
 	bool isCurrentPlayLayer(){
 		auto playLayer = geode::cocos::getChildOfType<PlayLayer>(cocos2d::CCScene::get(), 0);
@@ -84,7 +67,6 @@ class $modify(FixedPlayLayer, PlayLayer) {
 		    });
 	}
 };
-#ifndef GEODE_IS_WINDOWS
 class $modify(MyCCLayer, CCLayer){
 	void onEnter(){
 		if(reinterpret_cast<void*>(PlayLayer::get()) == reinterpret_cast<void*>(this)){
@@ -95,37 +77,19 @@ class $modify(MyCCLayer, CCLayer){
 		}
 	}
 };
-#endif
-/*class $modify(InfoLayer) {
-	void onMore(CCObject* sender){
-		auto s = static_cast<CCNode*>(sender);
-		auto infoLayer = static_cast<InfoLayer*>(s->getParent()->getParent()->getParent());
-		if(PlayLayer::get() && infoLayer->m_level->m_accountID.value() == 0){
-			FLAlertLayer::create("Error", "Sorry, but you can't view info about green player", "OK")->show();
-		} else {
-			InfoLayer::onMore(sender);
+class $modify(LevelInfoLayer) {
+	void onPlay(CCObject* s){
+		if(PlayLayer::get()){
+			FLAlertLayer::create("Error", "Can't open level while already in another level", "OK")->show();
+			return;
 		}
+		LevelInfoLayer::onPlay(s);
 	}
-	void onOriginal(CCObject* s){
-		if(!PlayLayer::get()){
-			InfoLayer::onOriginal(s);
+	void tryCloneLevel(CCObject* s){
+		if(PlayLayer::get()){
+			FLAlertLayer::create("Error", "Can't clone level while already in another level", "OK")->show();
+			return;
 		}
-	}
-};
-class $modify(ProfilePage) {
-	void onMyLevels(CCObject* s){
-		if(!PlayLayer::get()){
-			ProfilePage::onMyLevels(s);
-		} else {
-			FLAlertLayer::create("Error", "Sorry, but now you're not able to view player levels while playing level", "OK")->show();
-		}
-	}
-	void onMyLists(CCObject* s){
-		if(!PlayLayer::get()){
-			ProfilePage::onMyLists(s);
-		} else {
-			FLAlertLayer::create("Error", "Sorry, but now you're not able to view player lists while playing level", "OK")->show();
-		}
+		LevelInfoLayer:tryCloneLevel(s);
 	}
 };
-*/
