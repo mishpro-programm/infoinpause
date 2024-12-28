132ebcf3b153ba9fb4f4093d58f51f978e1cac4c
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-12-28 16:33:15 -0500

    ok i think i caught of them this time

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Geode/modify/PauseLayer.hpp>
+#include <Geode/modify/LevelInfoLayer.hpp>
 
 using namespace geode::prelude;
 
@@ -15,10 +16,27 @@ class $modify(MyPauseLayer, PauseLayer) {
 		menu->updateLayout();
 	}
 	void onCommentsButton(CCObject* sender) {
+		if (!CCScene::get()->getChildByType<PauseLayer>(0)) return;
 		const auto pl = PlayLayer::get();
 		if (!pl) return;
 		const auto level = pl->m_level;
 		if (!level) return;
-		InfoLayer::create(level, nullptr, nullptr)->show(); // the other two need to be nullptr so robtop knows which InfoLayer to generate
+		const auto infoLayer = InfoLayer::create(level, nullptr, nullptr);
+		if (!infoLayer) return;
+		infoLayer->show(); // the other two need to be nullptr so robtop knows which InfoLayer to generate
+	}
+};
+
+class $modify(LevelInfoLayer) {
+	void showError(const std::string& s) {
+		FLAlertLayer::create("Error", s, "OK")->show();
+	}
+	void onPlay(CCObject* s) {
+		if (PlayLayer::get()) return showError("Can't open level while already in another level");
+		LevelInfoLayer::onPlay(s);
+	}
+	void tryCloneLevel(CCObject* s) {
+		if (PlayLayer::get()) return showError("Can't clone level while already in another level");
+		LevelInfoLayer::tryCloneLevel(s);
 	}
 };
